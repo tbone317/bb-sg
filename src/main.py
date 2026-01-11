2c71d07501fa054f9077867b3991dae59593ffbd
Travis Dunbar <travis.j.dunbar@gmail.com>
2026-01-11 07:52:57 -0600

    more pages

--- src/main.py
+++ src/main.py
@@ -2,10 +2,12 @@ import os
 import shutil
 
 from copystatic import copy_files_recursive
-
+from gencontent import generate_page
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 
 def main():
@@ -22,5 +24,6 @@ def main():
         template_path,
         os.path.join(dir_path_public, "index.html"),
     )
-if __name__ == "__main__":
-    main()
+
+
+main()
